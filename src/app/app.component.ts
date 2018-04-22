6fc64d308c552b2d5cb013e0325b7fc4b254a73c
Angular CLI <angular-cli@angular.io>
2018-04-22 18:01:20 +0900

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lcs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lcs';
+}
